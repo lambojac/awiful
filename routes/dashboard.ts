ce6a77497be474d2ac203cfd0b30960edf631415
lambojac <adamokennie@gmail.com>
2025-02-11 03:34:40 +0100

    dashboard

--- routes/dashboard.ts
+++ routes/dashboard.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { getDashboardStats } from "../controller/dashboard";
+
 /**
  * @swagger
  * tags:
@@ -30,30 +31,6 @@ import { getDashboardStats } from "../controller/dashboard";
  *                   value:
  *                     type: integer
  *                     example: 1345
- *                   title:
- *                     type: string
- *                     example: "Projects"
- *                   value:
- *                     type: integer
- *                     example: 1345
- *                   title:
- *                     type: string
- *                     example: "User"
- *                   value:
- *                     type: integer
- *                     example: 13
- *                   title:
- *                     type: string
- *                     example: "Customer Estimates"
- *                   value:
- *                     type: integer
- *                     example: 13
- *                   title:
- *                     type: string
- *                     example: "Total Revenue"
- *                   value:
- *                     type: integer
- *                     example: 13
  *       500:
  *         description: Server error
  */
