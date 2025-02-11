0d4bee1e93b7e8ada830b2f3b8bb9a65b75bd3a1
lambojac <adamokennie@gmail.com>
2025-02-11 04:05:32 +0100

    analytics

--- dist/routes/projectAnalytics.js
+++ dist/routes/projectAnalytics.js
@@ -4,8 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const projectanalytics_1 = require("../controller/projectanalytics");
+const project_analytics_1 = require("../controller/project-analytics");
 const router = express_1.default.Router();
-router.get("/", projectanalytics_1.getProjectAnalytics);
+router.get("/", project_analytics_1.getProjectAnalytics);
 exports.default = router;
 //# sourceMappingURL=projectAnalytics.js.map
